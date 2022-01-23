1078156b2a0ad3ffd6f31e158ae676fa86fb1cdb
Kazumasa Utashiro <kaz@utashiro.com>
2022-01-23 13:45:46 +0900

    Add pip parameter.

--- pip-list.sh
+++ pip-list.sh
@@ -1,8 +1,10 @@
 #!/bin/bash
 
+pip=${PIP:-pip}
+
 for tools in $*
 do
-    show=$(pip show -f $tools)
+    show=$($pip show -f $tools)
     location=$(awk '$1 == "Location:" { print $2 }' <<< "$show")
     test "$location" != ''
     grep '^  ' <<< "$show" | sed "s%^  %${location}\/%" | perl -pE '1 while s{/[^/]+/\.\.(?=/)}{}'
